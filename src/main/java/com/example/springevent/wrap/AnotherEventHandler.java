a9a4a78305e27cabba9db46998bbe6a51a0e6688
jinye <whereyedo@naver.com>
2021-11-15 15:25:16 +0900

    MyEvent를 처리하는 다른 핸들러 만들기

---
+++ src/main/java/com/example/springevent/wrap/AnotherEventHandler.java
@@ -0,0 +1,13 @@
+package com.example.springevent.wrap;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnotherEventHandler {
+
+    @EventListener
+    public void onMyEvent(MyEvent event) {
+        System.out.println("Another event handling, data: " + event.getData());
+    }
+}
