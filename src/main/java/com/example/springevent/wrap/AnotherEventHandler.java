b707295839121e726883965eba03a38c7433dede
jinye <whereyedo@naver.com>
2021-11-15 15:40:57 +0900

    이벤트 처리 순서 설정

--- src/main/java/com/example/springevent/wrap/AnotherEventHandler.java
+++ src/main/java/com/example/springevent/wrap/AnotherEventHandler.java
@@ -1,12 +1,15 @@
 package com.example.springevent.wrap;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnotherEventHandler {
 
     @EventListener
+    @Order(Ordered.HIGHEST_PRECEDENCE + 2) // 이벤트 처리 순서 설정 - 설정하는 값이 클수록 우선순위가 낮아진다.
     public void onMyEvent(MyEvent event) {
         System.out.println(Thread.currentThread());
         System.out.println("Another event handling, data: " + event.getData());
--- src/main/java/com/example/springevent/wrap/MyEventHandler.java
+++ src/main/java/com/example/springevent/wrap/MyEventHandler.java
@@ -1,12 +1,15 @@
 package com.example.springevent.wrap;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyEventHandler {
 
     @EventListener
+    @Order(Ordered.HIGHEST_PRECEDENCE) // 이벤트 처리 순서 설정 - Ordered.HIGHEST_PRECEDENCE: 가장 높은 우선순위
     public void onMyEvent(MyEvent event) {
         System.out.println(Thread.currentThread());
         System.out.println("First event handling, data: " + event.getData());
