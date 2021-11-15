effc11c1cc0ab533cb6cae58c2ffa2297eafd347
jinye <whereyedo@naver.com>
2021-11-15 14:35:05 +0900

    이벤트 테스트

--- src/main/java/com/example/springevent/SpringEventApplication.java
+++ src/main/java/com/example/springevent/SpringEventApplication.java
@@ -1,13 +1,25 @@
 package com.example.springevent;
 
+import com.example.springevent.wrap.MyEventService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringEventApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringEventApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            MyEventService eventService = ctx.getBean(MyEventService.class);
+            eventService.run();
+        };
     }
 }
 
