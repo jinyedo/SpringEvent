1e9d124ad7a056b527744464456c0beab1e4c4c3
jinye <whereyedo@naver.com>
2021-11-15 15:07:58 +0900

    이벤트 클래스 변경

--- src/main/java/com/example/springevent/wrap/MyEvent.java
+++ src/main/java/com/example/springevent/wrap/MyEvent.java
@@ -1,23 +1,19 @@
 package com.example.springevent.wrap;
 
 import lombok.Getter;
-import org.springframework.context.ApplicationEvent;
 
-/*
-    Spring 4.2 미만에서는 이벤트를 만들기 위해 ApplicationEvent 를  상속받아야 한다.
-    Spring 에서 이벤트는 Bean 으로 등록하지 않는다.
- */
 @Getter
-public class MyEvent extends ApplicationEvent {
+public class MyEvent {
 
-    private int data;
+    private final Object source;
+    private final int data;
 
     public MyEvent(Object source) {
-        super(source);
+        this(source, 0);
     }
 
     public MyEvent(Object source, int data) {
-        super(source);
+        this.source = source;
         this.data = data;
     }
 }
